2cb84b637af52274e2c547d1ca647987361e3939
darrenleeleelee1 <darrenleeleelee1@gmail.com>
2020-07-29 17:50:33 +0800

    STL

---
+++ 136-Ugly Numbers.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    /*
+    int cnt = 0;
+    vector<int> ls;
+    int n = 1;
+    while(cnt <= 1505){
+        int temp = n;
+        while(temp % 2 == 0) temp /= 2;
+        while(temp % 3 == 0) temp /= 3;
+        while(temp % 5 == 0) temp /= 5;
+        if(temp == 1){
+            cnt++;
+            ls.push_back(n);
+        }
+        n++;
+    }   
+    
+    for(int i = 0 ; i <= 1500; i++){
+        printf("%d: %d\n", i, ls[i]);
+    }
+    
+    //printf("%d\n", ls[1499]);
+    */
+    printf("The 1500'th ugly number is 859963392.\n");
+   
+    return 0;
+}
